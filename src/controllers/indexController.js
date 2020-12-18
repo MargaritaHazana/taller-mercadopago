eee3a703168997d717bfd94aef2211815351bcc5
MargaritaHazana <margaritahazana@gmail.com>
2020-12-17 21:32:51 -0300

    Correciónes entrega

--- src/controllers/indexController.js
+++ src/controllers/indexController.js
@@ -16,7 +16,11 @@ module.exports = {
     callback: (req,res)=>{
         console.log(req.query);
         if (req.query.status.includes('success')){
-            return res.render('success');
+            return res.render('success', {
+                payment_type: req.query.payment_type,
+                external_reference: req.query.external_reference,
+                collection_id: req.query.collection_id
+            });
         }
 
         if (req.query.status.includes('pending')){
@@ -38,33 +42,32 @@ module.exports = {
                     id: '1234',
                     title: 'Product Name',
                     description: 'Dispositivo móvil de Tienda e-commerce',
-                    picture_url: '',
+                    picture_url: 'https://www.google.com/imgres?imgurl=https%3A%2F%2Fwww.muycomputerpro.com%2Fwp-content%2Fuploads%2F2014%2F01%2Faplicaciones-moviles.jpg&imgrefurl=https%3A%2F%2Fwww.muycomputerpro.com%2F2014%2F01%2F15%2Fingresos-dispositivos-moviles&tbnid=o9Z-bSNtW22kuM&vet=12ahUKEwi3_OmoodbtAhXlB9QKHb0HA0YQMygAegUIARCrAQ..i&docid=U0zlClllyNsm-M&w=630&h=473&q=dispositivo%20movil&ved=2ahUKEwi3_OmoodbtAhXlB9QKHb0HA0YQMygAegUIARCrAQ',
                     unit_price: 10,
-                    quantity: 1,
-                    currency_id: 'ARS'
+                    quantity: 1
                 }
             ],
             payment_methods: {
-                installments: 12,
+                installments: 6,
                 excluded_payment_types: [
                     {id: 'atm'}
                 ],
                 excluded_payment_methods: [
-                    {id: 'visa'}
+                    {id: 'amex'}
                 ]
             },
             payer: {
-                name: 'Ryan',
-                suname: 'Dahl',
+                name: 'Lalo',
+                suname: 'Landa',
                 mail: 'test_user_63274575@testuser.com',
                 phone: {
                     area_code: '11',
-                    number: 55556666
+                    number: 22223333
                 },
-                adress: {
-                    zip_code: '1234',
-                    street_name: 'Monroe',
-                    street_number: 860
+                address: {
+                    zip_code: '1111',
+                    street_name: 'False',
+                    street_number: 123
                 }
             },
             external_reference: 'margaritahazana@gmail.com',
